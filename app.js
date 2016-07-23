946f34dd20669af0cfb3221f7808e013f82c4365
Vinod S R <mailme@vinodsr.com>
2016-07-23 18:22:42 +0530

    added webpage to display the list using shuffle method .

--- app.js
+++ app.js
@@ -2,15 +2,52 @@ var express = require("express");
 var app = express();
 var Random = require("random-js");
 var random = new Random(Random.engines.mt19937().autoSeed());
-var value = random.integer(1, 100);
+var jsonfile = require('jsonfile');
 
-var team = ["Team 1", "Team 2", "team 3"];
+app.use(express.static('static'));
 app.get("/generate", function(req, res) {
-  res.json(random.shuffle(team));
+
+  jsonfile.readFile("./config.json", function(err, data) {
+    if (err) {
+      console.log(err)
+      res.json(prepareMessage(1, err));
+      return;
+    }
+
+    var randomMembers = random.shuffle(data.members);
+    var teams = data.teams;
+    var teamcount = teams.length;
+    var result = {};
+    var currentTeamIdx = 0;
+    var currentTeam = "";
+    randomMembers.forEach(function(member) {
+      currentTeam = teams[currentTeamIdx];
+      if (typeof result[currentTeam] === "undefined") {
+        result[currentTeam] = [];
+      }
+      result[currentTeam].push(member);
+
+      currentTeamIdx++;
+      if (currentTeamIdx >= teams.length) {
+        currentTeamIdx = 0;
+      }
+    });
+
+    res.json(result);
+  });
+
 });
-// start server 
+// start server
 app.listen(3000, function() {
   console.log("Server started ");
 
 
 });
+
+
+var prepareMessage = function(errcode, message) {
+  return {
+    statusCode: errcode,
+    statusMessage: message
+  };
+};
